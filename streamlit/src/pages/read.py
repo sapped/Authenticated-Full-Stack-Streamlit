e68bbd247876f2723cf8ba1612d4f57364cf43d4
Ed <edwardsapp@gmail.com>
2020-12-27 15:17:02 +0000

    moved authentication just to / endpoint

--- streamlit/src/pages/read.py
+++ streamlit/src/pages/read.py
@@ -11,12 +11,14 @@ PAGE_TITLE = 'Read Items'
 def write():
     st.markdown(f'# {PAGE_TITLE}')
     
-    read_items()
+    df = read_items()
+    st.write(df)
 
 def read_items():
     url = f'{API_URL}/items/'
     res = requests.get(url)
-    st.write(pd.read_json(res.text))
+    df = pd.read_json(res.text)
+    return df.set_index('id')
     
     # if you just want to see the JSON, uncomment this:
     # res_json = json.loads(res.text)
